cdde6f5a2a608f76de738041ce8be208a7951daa
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-26 16:41:02 -0600

    add second vendor test failing pass

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Tests.ModelTests
 {
@@ -11,6 +12,26 @@ namespace VendorOrderTracker.Tests.ModelTests
         {
             Vendor newVendor = new Vendor("Suzie's Cafe", "Local cafe");
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+        [TestMethod]
+        public void AddOrder_AssociatesOrderWithVendor_OrderList()
+        {
+        
+            string title = "Pastry Order";
+            string description = "Croissants";
+            int price = 18;
+            string date = "2024-01-24";
+            Order newOrder = new Order(title, description, price, date);
+            List<Order> newList = new List<Order> { newOrder };
+            Vendor newVendor = new Vendor("Suzie's Cafe", "Local cafe");
+
+            
+            newVendor.AddOrder(newOrder);
+            List<Order> result = newVendor.Orders;
+
+            
+            CollectionAssert.AreEqual(newList, result);
         }
     }
 }

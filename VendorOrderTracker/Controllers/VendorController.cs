f95b08fa5d69c28a56043901ff2550e9c01c170e
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-27 17:13:35 -0600

    provides action method for classes spcefically vendors

---
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+  }
+}

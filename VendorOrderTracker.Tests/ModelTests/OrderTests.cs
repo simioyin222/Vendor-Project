3ae77de48035a261ca69f1a1a05373f5b1c26562
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-26 18:12:16 -0600

    add second test to order function good fail

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -13,5 +13,24 @@ namespace VendorOrderTracker.Tests
             Order newOrder = new Order("Order Title", "Description", 10.99, DateTime.Now);
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+
+        [TestMethod]
+        public void GetProperties_ReturnsProperties_PropertyValues()
+        {
+            // Arrange
+            string title = "Order Title";
+            string description = "Description";
+            double price = 10.99;
+            DateTime date = DateTime.Now;
+
+            // Act
+            Order newOrder = new Order(title, description, price, date);
+
+            // Assert
+            Assert.AreEqual(title, newOrder.Title);
+            Assert.AreEqual(description, newOrder.Description);
+            Assert.AreEqual(price, newOrder.Price);
+            Assert.AreEqual(date, newOrder.Date);
+        }
     }
 }

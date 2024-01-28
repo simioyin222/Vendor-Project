85d1073137de593289104fb0b922fa03189930d5
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-28 03:21:43 -0600

    null addition ruined page

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -10,10 +10,6 @@ namespace VendorOrderTracker.Controllers
         public ActionResult New(int vendorId)
         {
             Vendor vendor = Vendor.Find(vendorId);
-            if (vendor == null)
-            {
-               return NotFound();
-            }
             return View(vendor);
         }
 

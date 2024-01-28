b72049312b4c1d8aef574c9f8811ae5c7b253fbf
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-28 15:00:03 -0600

    change syntax again

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -10,7 +10,7 @@ namespace VendorOrderTracker.Controllers
         public ActionResult New(int vendorId)
         {
             Vendor vendor = Vendor.Find(vendorId);
-            return View(vendor);
+            return View("New", vendor);
         }
 
         [HttpPost("/vendors/{vendorId}/orders")]

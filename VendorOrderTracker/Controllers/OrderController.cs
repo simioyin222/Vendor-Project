ec56926f6c7f726015ebb2f3e059127b1912a1f5
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-27 22:11:33 -0600

    better phrase order controller for date and time

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -1,25 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorOrderTracker.Models;
-using System.Collections.Generic;
+using System;
 
 namespace VendorOrderTracker.Controllers
 {
-  public class OrderController : Controller
-  {
-    [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
+    public class OrderController : Controller
     {
-      Vendor foundVendor = Vendor.Find(vendorId);
-      return View(foundVendor);
-    }
+        [HttpGet("/vendors/{vendorId}/orders/new")]
+        public ActionResult New(int vendorId)
+        {
+            Vendor vendor = Vendor.Find(vendorId);
+            return View(vendor);
+        }
 
-    [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
-    {
-      Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
-      foundVendor.AddOrder(newOrder);
-      return RedirectToAction("Show", "Vendor", new { id = vendorId });
+        [HttpPost("/vendors/{vendorId}/orders")]
+        public ActionResult Create(int vendorId, string title, string description, double price, string date)
+        {
+            DateTime orderDate = DateTime.Parse(date);  // Convert string to DateTime
+            Order newOrder = new Order(title, description, price, orderDate);
+            Vendor vendor = Vendor.Find(vendorId);
+            vendor.AddOrder(newOrder);
+            return RedirectToAction("Show", "Vendor", new { id = vendorId });
+        }
     }
-  }
 }

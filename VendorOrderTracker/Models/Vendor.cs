c1656d172c2300888cd027dc2b02fb5d4587a37f
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-27 22:02:08 -0600

    fix how app reads vendor order

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -5,17 +5,31 @@ namespace VendorOrderTracker.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Order> Orders { get; set; }
+        public int Id { get; }
+        private static List<Vendor> _instances = new List<Vendor> {};
 
         public Vendor(string name, string description)
         {
             Name = name;
             Description = description;
             Orders = new List<Order>();
+            _instances.Add(this);
+            Id = _instances.Count;
         }
 
         public void AddOrder(Order order)
         {
             Orders.Add(order);
+        }
+
+        public static List<Vendor> GetAll()
+        {
+            return _instances;
+        }
+
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId - 1];
         }
     }
 }
